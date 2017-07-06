a8946436a73478474b053445ca5e4d36705f596c
funny8888 <helloserinacolleges@gmail.com>
2017-07-06 14:47:39 -0700

    Update port.js

--- port.js
+++ port.js
@@ -2,11 +2,11 @@ function shapes(num)
 {
    if (num == 0)
    {
-      document.getElementById('shapes_display').src = 'Shapes_display1.png'
+      document.getElementById('shapes_display').innerHTML = 'Shapes_display1.png'
    }
    else
    {
-      document.getElementById('shapes_display').src = 'Shapes_display2.png'
+      document.getElementById('shapes_display').innerHTML = 'Shapes_display2.png'
    }
 }
 
