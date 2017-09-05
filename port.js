753470c72fcde267aa45f8e85d108765e524ba0b
funny8888 <helloserinacolleges@gmail.com>
2017-09-04 20:12:34 -0700

    some updates

--- port.js
+++ port.js
@@ -2,11 +2,11 @@ function shapes(num)
 {
    if (num == 0)
    {
-      document.getElementById('shapes_display').innerHTML = 'https://funny8888.github.io/Portfolio/Photos/Shapes_display1.png'
+      document.getElementById('shapes_display').innerHTML = 'Photos/Shapes_display1.png'
    }
    else
    {
-      document.getElementById('shapes_display').innerHTML = 'https://funny8888.github.io/Portfolio/Photos/Shapes_display2.png'
+      document.getElementById('shapes_display').innerHTML = 'Photos/Shapes_display2.png'
    }
 }
 
